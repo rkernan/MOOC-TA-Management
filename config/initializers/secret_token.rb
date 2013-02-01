63cb7e74c5e7fbee0cc3ba7773676ef19477fd91
Robert Kernan <rkernan@gmail.com>
2013-02-01 10:19:30 -0500

    initialized rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoocTaManager::Application.config.secret_token = '62c7abf9d7eab09db54c979d21b298e5ccfa04d22167655218d16affd03eb8def52cbede8e6e4a8747c41c7678dbdcc3fff5d4d522c6edb918c5f294751cc5d7'
