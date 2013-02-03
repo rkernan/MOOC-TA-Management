3ca1184160f32e8038b2a9ad5a427f668cd4f14a
Doug Black <dougblack@gatech.edu>
2013-02-03 13:31:55 -0500

    Added a comment describing :has_many.

--- app/models/ta_test.rb
+++ app/models/ta_test.rb
@@ -1,7 +1,14 @@
 class TaTest < ActiveRecord::Base
 
-    has_many :questions
+  # This tells Rails that the 'Question' table [should] contain
+  # a reference to a TaTest. This let's us call ta_test_object.questions
+  # to get a list of all the questions belonging to this test. It's
+  # important to note that the actual 'TaTest' table DOES NOT contain
+  # any references to Questions. When you call .questions(), Rails
+  # knows to do a query on the 'Question' table for records with a 
+  # 'ta_test_id that' matches the calling objects id.
+  has_many :questions
 
-    belongs_to :course
-    attr_accessible :name
+  belongs_to :course
+  attr_accessible :name
 end
