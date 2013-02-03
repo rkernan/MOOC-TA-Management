0931ff8b0f9f13928b101f3f77ef8623aae863df
Robert Kernan <rkernan@gmail.com>
2013-02-03 11:45:53 -0500

    added user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 MoocTaManager::Application.routes.draw do
-  get "users/index"
+  resources :users
+  # get "users/index"
 
-  get "users_controller/index"
+  # get "users_controller/index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
