14050736ad4c62b41daa03dc7e2ed167fd169c95
Robert Kernan <rkernan@gmail.com>
2013-04-02 23:48:56 -0400

    Fixed courses new form.

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,12 @@
 MoocTaManager::Application.routes.draw do
   root :to => 'users#index'
+
   get 'register' => 'users#new', :as => 'register'
   get 'login' => 'sessions#login', :as => 'login'
   get 'logout' => 'sessions#logout', :as => 'logout'
-
   post 'sessions/authenticate' => 'sessions#authenticate'
 
-  get 'users' => 'users#index'
-  post 'users' => 'users#create', :as => 'create_user'
-  get 'users/new' => 'users#new', :as => 'new_user'
-  get 'users/:id' => 'users#show', :as => 'user'
-  get 'users/:id/edit' => 'users#edit', :as => 'edit_user'
-  put 'users/:id' => 'users#update'
-  delete 'users/:id' => 'users#destroy'
+  resources :users
 
   resources :teaching_assistants, :controller => 'users'
 
