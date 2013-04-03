d9e18179dc2a4eff20285a21556897edcc9a77ff
PratikDeshpande <pratik661@gmail.com>
2013-04-02 21:09:57 -0400

    Modified test_resultsController def index

--- app/controllers/test_results_controller.rb
+++ app/controllers/test_results_controller.rb
@@ -1,2 +1,16 @@
 class TestResultsController < ApplicationController
+
+  def index
+    @test_results = TestResult.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @test_results }
+  end
+
+  #def create
+   # @test_results = TestResult.find(params[:ta_test_id])
+   # @question = @ta_test.questions.create(params[:question])
+   # redirect_to ta_test_path(@ta_test)
+  #end
+
 end
