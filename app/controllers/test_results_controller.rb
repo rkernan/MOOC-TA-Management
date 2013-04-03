fc6b25ed38c8e02c12f8f10858a0e34a58595d80
Matt Howard <matrixmickey@gmail.com>
2013-04-02 21:36:46 -0400

    Hey

--- app/controllers/test_results_controller.rb
+++ app/controllers/test_results_controller.rb
@@ -1,2 +1,12 @@
 class TestResultsController < ApplicationController
+
+  def create
+    @ta_test = TaTest.find(params[:ta_test_id])
+    @ta = TeachingAssistant.find(params[:teaching_assistant_id]
+    @result = @ta_test.test_results.create(params[:test_result])
+    @result = @ta.test_results.update(params[:test_result])
+    redirect_to teaching_assistant_test_result_path(@result)
+  end
+
+
 end
