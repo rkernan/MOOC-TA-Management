faa69bbf3aaaa08addb02e890182282877cd4bfe
PratikDeshpande <pratik661@gmail.com>
2013-04-08 09:20:47 -0400

    Adding link to all test results on professor show.html.erb

--- app/controllers/test_results_controller.rb
+++ app/controllers/test_results_controller.rb
@@ -2,14 +2,16 @@ class TestResultsController < ApplicationController
 
   def create
     @ta_test = TaTest.find(params[:ta_test_id])
-    @ta = TeachingAssistant.find(params[:teaching_assistant_id]
+    @ta = TeachingAssistant.find(params[:teaching_assistant_id])
     @result = @ta_test.test_results.create(params[:test_result])
     @result = @ta.test_results.update(params[:test_result])
     redirect_to teaching_assistant_test_result_path(@result)
   end
 
   def index
-    @test_results = TestResult.all
+    @ta_test = TaTest.find(params[:ta_test_id])
+   # @ta = TeachingAssistant.find(params[:teaching_assistant_id])
+    @test_results = @ta_test.test_results.all
     respond_to do |format|
       format.html
       format.json { render json: @test_results }
