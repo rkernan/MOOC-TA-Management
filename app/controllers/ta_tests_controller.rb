69750701b4c5279217945319effcef891cda6675
Robert Kernan <rkernan@gmail.com>
2013-04-17 12:59:25 -0400

    Pages now authenticate on user id.

--- app/controllers/ta_tests_controller.rb
+++ app/controllers/ta_tests_controller.rb
@@ -1,6 +1,15 @@
 class TaTestsController < ApplicationController
-  before_filter :require_professor, :only => [:new, :create, :edit, :update, :show]
   before_filter :require_teaching_assistant, :only => [:take]
+  before_filter :require_professor, :only => [:new, :create, :edit, :update, :show]
+  before_filter :only => [:new, :create, :edit, :update, :show] {
+    require_specific_user(
+      if params[:course_id]
+        Course.find(params[:course_id]).professor
+      else
+        TaTest.find(params[:id]).course.professor
+      end
+    )
+  }
 
   # GET /ta_tests
   # GET /ta_tests.json
