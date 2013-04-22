d55c2d501d4c3a8f2c165f2a00c89f16253e6cc3
Robert Kernan <rkernan@gmail.com>
2013-04-22 19:20:42 -0400

    Added test_results creation to controller. Broken.

--- app/controllers/test_results_controller.rb
+++ app/controllers/test_results_controller.rb
@@ -1,5 +1,41 @@
 class TestResultsController < ApplicationController
 
+  # GET /test_results
+  # GET /test_results.json
+  def index
+    @ta_test = TaTest.find(params[:ta_test_id])
+    @test_results = @ta_test.test_results.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @test_results }
+    end
+  end
+
+  # GET /test_results/new
+  # GET /test_results/new.json
+  def new
+    @ta_test = TaTest.find(params[:ta_test_id])
+    @ta = TeachingAssistant.find(current_user.id)
+    @test_result = @ta_test.test_results.new(:teaching_assistant => @ta)
+
+    @ta_test.questions.each do |question|
+      question_result = @test_result.question_results.build
+      question_result.question = question
+      question.answers.each do |answer|
+        answer_result = question_result.answer_results.build
+        answer_result.answer = answer
+      end
+    end
+
+    respond_to do |format|
+      format.html
+      format.json { render json: [@ta_test, @test_result] }
+    end
+  end
+
+  # POST /test_results
+  # POST /test_results.json
   def create
     @ta_test = TaTest.find(params[:ta_test_id])
     @test_result = @ta_test.test_results.new(params[:test_result])
@@ -10,45 +46,9 @@ class TestResultsController < ApplicationController
         format.json { render json: @test_result, status: :created, location: @test_result }
       else
         format.html { render action: "new" }
-        format.json { render json: @test_result.errros, status: :unprocessable_entity }
+        format.json { render json: @test_result.errors, status: :unprocessable_entity }
       end
     end
   end
-
-  def index
-   # @ta_test = TaTest.find(params[:ta_test_id])
-   # @ta = TeachingAssistant.find(params[:teaching_assistant_id])
-   # @test_results = @ta.test_results.all
-    @professor = Professor.find(params[:professor_id])
-    @course = Course.find(params[:course_id])
-    @ta_test = TaTest.find(params[:ta_test_id])
-    @test_results = @ta_test.test_results.all
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @test_results }
-    end
-  end
-
-  def new
-    @ta_test = TaTest.find(params[:ta_test_id])
-    @ta = TeachingAssistant.find(current_user.id)
-    @test_result = @ta_test.test_results.new
-
-   # @ta_test.questions.each do |question|
-   #   question_result = @test_result.question_results.build
-   #   question_result.question = question
-   #   question.answers.each do |answer|
-   #     answer_result = question_result.answer_results.build
-   #     answer_result.answer = answer
-   #   end
-   # end
-
-    respond_to do |format|
-      format.html
-      format.json { render json: [@ta_test, @test_result] }
-    end
-  end
-
 
 end
--- app/models/test_result.rb
+++ app/models/test_result.rb
@@ -1,9 +1,9 @@
 class TestResult < ActiveRecord::Base
   belongs_to :teaching_assistant
   belongs_to :ta_test
-  has_many :question_results
-  has_many :answer_results, :through => :question_results
-  attr_accessible :question_results_attributes, :answer_results_attributes
+  has_many :question_results, :dependent => :destroy
+  attr_accessible :question_results_attributes, :teaching_assistant, :ta_test
   accepts_nested_attributes_for :question_results
-  accepts_nested_attributes_for :answer_results
+  # validates :teaching_assistant, :presence => true
+  validates :ta_test, :presence => true
 end
