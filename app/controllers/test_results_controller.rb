c60d294894ebf98f767c7a3b158ece6496c1b2e1
Matt Howard <matrixmickey@gmail.com>
2013-04-23 05:19:31 -0400

    Creates test_result but the references from question_result to question and answer_result to answer don't get set like we want

--- app/controllers/test_results_controller.rb
+++ app/controllers/test_results_controller.rb
@@ -19,12 +19,16 @@ class TestResultsController < ApplicationController
     @ta = TeachingAssistant.find(current_user.id)
     @test_result = @ta_test.test_results.new(:teaching_assistant => @ta)
 
+    @qs_and_as = Array.new
+
     @ta_test.questions.each do |question|
       question_result = @test_result.question_results.build
       question_result.question = question
+      @qs_and_as.push(question.content)
       question.answers.each do |answer|
         answer_result = question_result.answer_results.build
         answer_result.answer = answer
+        @qs_and_as.push(answer.content)
       end
     end
 
@@ -38,14 +42,17 @@ class TestResultsController < ApplicationController
   # POST /test_results.json
   def create
     @ta_test = TaTest.find(params[:ta_test_id])
+    @course = @ta_test.course
     @test_result = @ta_test.test_results.new(params[:test_result])
+    @test_result.teaching_assistant = TeachingAssistant.find(current_user.id)
 
     respond_to do |format|
       if @test_result.save
         format.html { redirect_to @test_result, notice: 'Test was successfully taken.' }
         format.json { render json: @test_result, status: :created, location: @test_result }
       else
-        format.html { render action: "new" }
+        # format.html { redirect_to course_ta_tests_path(@course), notice: 'Test could not be taken.' }
+        format.html { render json: @test_result.errors, status: :unprocessable_entity }
         format.json { render json: @test_result.errors, status: :unprocessable_entity }
       end
     end
--- app/models/question_result.rb
+++ app/models/question_result.rb
@@ -4,6 +4,6 @@ class QuestionResult < ActiveRecord::Base
   has_many :answer_results, :dependent => :destroy
   attr_accessible :correct, :answer_results_attributes, :question, :test_result
   accepts_nested_attributes_for :answer_results
-  validates :test_result, :presence => true
-  validates :answer, :presence => true
+#  validates :test_result, :presence => true
+#  validates :question, :presence => true
 end
