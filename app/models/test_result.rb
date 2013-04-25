402a094dac8e7b47e5127066ba88838dba473579
Robert Kernan <rkernan@gmail.com>
2013-04-25 01:02:56 -0400

    Added error messages.

--- app/models/test_result.rb
+++ app/models/test_result.rb
@@ -4,6 +4,6 @@ class TestResult < ActiveRecord::Base
   has_many :question_results, :dependent => :destroy
   attr_accessible :question_results_attributes, :teaching_assistant, :ta_test
   accepts_nested_attributes_for :question_results
-  # validates :teaching_assistant, :presence => true
+  validates :teaching_assistant, :presence => true
   validates :ta_test, :presence => true
 end
