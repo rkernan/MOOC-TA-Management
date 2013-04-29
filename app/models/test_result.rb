084269cc2d46610d8068818ff968c27f81ce5a40
amsharm <asharma80@gatech.edu>
2013-04-29 15:35:45 -0400

    a

--- app/models/test_result.rb
+++ app/models/test_result.rb
@@ -2,7 +2,7 @@ class TestResult < ActiveRecord::Base
   belongs_to :teaching_assistant
   belongs_to :ta_test
   has_many :question_results, :dependent => :destroy
-  attr_accessible :question_results_attributes, :teaching_assistant, :ta_test, :num_questions, :num_correct_answers
+  attr_accessible :question_results_attributes, :teaching_assistant, :ta_test, :num_questions, :num_correct_answers, :percentage_correct
   accepts_nested_attributes_for :question_results
   # validates :teaching_assistant, :presence => true
   validates :ta_test, :presence => true
